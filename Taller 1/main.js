061f6ef8c09bc5b8f6a5b1a4310dc3045945878e
Juan <j.toledo05@ufromail.cl>
2021-04-25 15:51:44 -0400

    Update main.js

--- Taller 1/main.js
+++ Taller 1/main.js
@@ -12,37 +12,26 @@ var display4 = document.getElementById("display4");
 let comentarios = [];
 let autores = [];
 let displays = [document.getElementById("display1"), document.getElementById("display2"), document.getElementById("display3"), document.getElementById("display4")]
-
 boton.onclick = function() {
     const tiempoTranscurrido = Date.now();
     const hoy = new Date(tiempoTranscurrido);
     console.log(display1.innerHTML);
-    /* comentarios = comentarios.push(contenido.value); */
     if (autor.value.toString().length > 2 && contenido.value.toString().length != 0) {
         if (comentarios.length < 4) {
             autores.unshift(autor.value);
             comentarios.unshift(contenido.value);
             comentarios.forEach((com, index) => {
-
                 (displays[index]).innerHTML =  `<div class="display"><div><h2>${autores[index]}</h2><span class="fecha">${hoy.toLocaleDateString()}</span></div><div class="comentario">${com}</div>  </div> `;
             });
-
-
-
         } else {
-
-
             autores.unshift(autor.value);
             comentarios.unshift(contenido.value);
             autores.pop();
             comentarios.pop();
             console.log(" =====");
             comentarios.forEach((com, index) => {
-
                 (displays[index]).innerHTML =  `<div class="display"><div><h4>${autores[index]}</h4></div>${com}<div></div>  </div> `;
-
                 console.log(index + 1, com);
-
             });
         }
     } else if (autor.value.toString().length <= 2) {
